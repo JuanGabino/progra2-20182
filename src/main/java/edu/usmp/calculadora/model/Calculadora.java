aca11206863c447ed62412d6480413da9c01c21f
user <user@192.168.1.39>
2018-03-17 00:10:18 -0500

    version 1.0

---
+++ src/main/java/edu/usmp/calculadora/model/Calculadora.java
@@ -0,0 +1,21 @@
+package edu.usmp.calculadora.model;
+
+public class Calculadora {
+
+	private Double nro1;
+	private Double nro2;
+	
+	public Double getNro1() {
+		return nro1;
+	}
+	public void setNro1(Double nro1) {
+		this.nro1 = nro1;
+	}
+	public Double getNro2() {
+		return nro2;
+	}
+	public void setNro2(Double nro2) {
+		this.nro2 = nro2;
+	}
+	
+}
---
+++ src/main/java/edu/usmp/calculadora/web/CalculadoraController.java
@@ -0,0 +1,28 @@
+package edu.usmp.calculadora.web;
+//importar dependencias
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import edu.usmp.calculadora.model.Calculadora;
+
+@Controller
+public class CalculadoraController {
+
+	@GetMapping("/new")
+	public String initCreationForm(Model model) {
+		model.addAttribute("calculadora", new Calculadora());
+		return "calculadoraForm";
+	}
+
+	@PostMapping("/new")
+	public String submitForm(@ModelAttribute Calculadora calculadora) {
+		
+		//calculadora.result;
+		
+		return "resultForm";
+	}
+}
